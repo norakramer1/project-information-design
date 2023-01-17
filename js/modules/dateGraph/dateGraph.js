8f9904cc7dc7778aa5ce98e447adab4038b0907f
nora kramer <74241562+norakramer1@users.noreply.github.com>
2023-01-17 13:43:43 +0100

    dates logged

--- js/modules/dateGraph/dateGraph.js
+++ js/modules/dateGraph/dateGraph.js
@@ -7,8 +7,23 @@ export function dateGraph(dates) {
     let something = dates.forEach(el => {
     //   console.log(el)
       answers.push(el.textContent)
+
+
+
     })
-    
-    console.log(something)
     console.log(answers)
+
+    const regex = new RegExp(/^([0-9]{4})?$/);
+const str = "oudste stuk - jongste stuk: 1594-1603";
+   // console.log(regex)
+  
+   let newStr = str.replace(/\D/g,'');
+    const matches = answers.map(answer => answer.replace(/\D/g,''));
+   //const matches = answers.match(regex);
+    console.log(matches);
+    
+   // console.log(something)
+
 }
+
+// REGEX: 
